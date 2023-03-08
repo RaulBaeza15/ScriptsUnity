dacee9eac0c0ced22c9e7ea2d51f46340bdc8bc8
Raul <raulbaeza8@gmail.com>
2023-03-08 14:41:58 +0100

    script que mira a cube y a cube(1)

--- Scripts/MovimientoCamara.cs
+++ Scripts/MovimientoCamara.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MovimientoCamara : MonoBehaviour
 {
+    int numeroObjeto=0;
+    int totalObjetos=2;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,20 @@ public class MovimientoCamara : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.G)){
+            GameObject cam = GameObject.Find("Main Camera");
+            GameObject cubo = GameObject.Find("Cube");
+            GameObject cubo2 = GameObject.Find("Cube (1)");
+            if(numeroObjeto%totalObjetos==0){
+                cam.transform.LookAt(cubo.transform);
+            }else{
+               cam.transform.LookAt(cubo2.transform); 
+            }
+            
+
+            
+
+            numeroObjeto++;
+        }
     }
 }
