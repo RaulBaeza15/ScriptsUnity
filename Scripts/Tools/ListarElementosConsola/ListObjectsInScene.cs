419b1dd8fdc1f6f859c17144b662de09f897fa41
Raul <raulbaeza8@gmail.com>
2023-03-17 13:28:11 +0100

    listar objetos por pantalla

---
+++ Scripts/Tools/ListarElementosConsola/ListObjectsInScene.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ListObjectsInScene : EditorWindow
+{
+    [MenuItem("Tools/List Objects in Scene")]
+    public static void ShowWindow()
+    {
+        // Create window
+        EditorWindow window = EditorWindow.GetWindow(typeof(ListObjectsInScene));
+        window.titleContent = new GUIContent("List Objects in Scene");
+
+        // Get all game objects in the scene
+        GameObject[] allObjects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+
+        // Display each object's name in the console
+        foreach (GameObject obj in allObjects)
+        {
+            Debug.Log(obj.name);
+        }
+    }
+}
